12528e26f632192d544fa7c99f170bd3e83040e6
Aditya <adityasara2622@gmail.com>
2025-12-12 22:09:46 +0530

    message

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ form.addEventListener('submit', function(e) { // add submit event listener
 const height = parseInt(document.querySelector('#height').value); // get height value and convert to integer
 const weight = parseInt(document.querySelector('#weight').value); // get weight value and convert to integer
 const result = document.querySelector('#result');// get the result div
+const message = document.querySelector('#message');// get the message div
 
 // validate the input
 
@@ -18,7 +19,18 @@ if (isNaN(height) || height <= 0 || height ==='') { // check if height is a vali
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);// BMI formula
     // show the result
     result.innerHTML = `<span>${bmi}</span>`;       // display BMI value
+if(bmi < 18.6){
+    message.innerHTML =`<span>you are under weight</span>`; // display underweight message
 }
-
+else if(bmi >= 18.6 && bmi < 24.9){
+    message.innerHTML =`<span>you are normal weight</span>`; // display normal weight message
+}
+else if(bmi >= 24.9 && bmi < 29.9){
+    message.innerHTML =`<span>you are over weight</span>`; // display overweight message
+}
+else{
+    message.innerHTML =`<span>you are obese</span>`; // display obese message
+}
+}
 
 })
